c4f7d696222f7fa7fdaacd64d60558465a07b013
Filip Wachowiak <filipw01@interia.eu>
2021-12-05 10:35:02 +0100

    Day 5

---
+++ rust/src/day5/task2.rs
@@ -0,0 +1,71 @@
+use crate::utils::load_input;
+use itertools::Itertools;
+
+#[derive(Debug)]
+struct Vent {
+    x1: usize,
+    y1: usize,
+    x2: usize,
+    y2: usize,
+}
+
+pub fn solve() -> usize {
+    let lines = load_input(5);
+    let mut ocean_floor = [[0; 1000]; 1000];
+    lines
+        .into_iter()
+        .map(|line| {
+            let (start, end) = line.split_once(" -> ").unwrap();
+            let (x1, y1) = start.split_once(",").unwrap();
+            let (x2, y2) = end.split_once(",").unwrap();
+            Vent {
+                x1: x1.parse().unwrap(),
+                y1: y1.parse().unwrap(),
+                x2: x2.parse().unwrap(),
+                y2: y2.parse().unwrap(),
+            }
+        })
+        .for_each(|vent| {
+            if vent.x1 == vent.x2 {
+                let range = if vent.y1 < vent.y2 {
+                    vent.y1..vent.y2 + 1
+                } else {
+                    vent.y2..vent.y1 + 1
+                };
+                for y in range {
+                    ocean_floor[y][vent.x1] += 1
+                }
+            } else if vent.y1 == vent.y2 {
+                let range = if vent.x1 < vent.x2 {
+                    vent.x1..vent.x2 + 1
+                } else {
+                    vent.x2..vent.x1 + 1
+                };
+                for x in range {
+                    ocean_floor[vent.y1][x] += 1
+                }
+            } else {
+                let y = if vent.x1 < vent.x2 { vent.y1 } else { vent.y2 };
+                let up = if vent.x1 < vent.x2 {
+                    vent.y1 > vent.y2
+                } else {
+                    vent.y1 < vent.y2
+                };
+                let range = if vent.x1 < vent.x2 {
+                    vent.x1..vent.x2 + 1
+                } else {
+                    vent.x2..vent.x1 + 1
+                };
+                for (index, x) in range.enumerate() {
+                    let y = if up { y - index } else { y + index };
+                    ocean_floor[y][x] += 1
+                }
+            };
+        });
+    let crossings_count = ocean_floor
+        .iter()
+        .map(|row| row.iter().filter(|cell| **cell > 1).count())
+        .sum();
+
+    crossings_count
+}
